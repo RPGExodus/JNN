19e80799a9b43954a46f0ca4f493e23e2be7e4b2
RPGExodus <Raver98@wp.pl>
2017-06-13 18:18:01 +0200

    s

--- app/src/main/java/pl/billog_studio/janigdynie_graimprezowa/Pytania_usage.java
+++ app/src/main/java/pl/billog_studio/janigdynie_graimprezowa/Pytania_usage.java
@@ -1,5 +1,25 @@
 package pl.billog_studio.janigdynie_graimprezowa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Pytania_usage {
-    
+
+    String[] baza={"Ja nigdy nie byłem w sex-shopie",
+            "Ja nigdy nie pływałem w morzu",
+            "Ja nigdy nie byłem w klubie ze striptizem",
+            "Ja nigdy nie piłem teqilli"};
+    ArrayList<String> pytania=new ArrayList();
+
+    public String WybierzLosowePytanie()
+    {
+        Random los = new Random();
+        return pytania.remove(los.nextInt(pytania.size()));
+    }
+    public Pytania_usage()
+    {
+        pytania.addAll(Arrays.asList(baza));
+    }
 }
+
